b1aca84b5d9d5d76463d60900624d0587da938ec
Juan Gedler <jgedler@gmail.com>
2025-06-26 21:37:05 -0400

    Initial Commit

---
+++ DangerSample/DangerSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  DangerSampleApp.swift
+//  DangerSample
+//
+//  Created by Juan Gedler on 26-06-25.
+//
+
+import SwiftUI
+
+@main
+struct DangerSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
