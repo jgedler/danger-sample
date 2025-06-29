0d69b54694baa74ed3a349465932c0e7be30a992
Juan Gedler <jgedler@gmail.com>
2025-06-28 21:26:09 -0400

    adds linting rules

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -1,5 +1,9 @@
 import Danger 
+import DangerSwiftLint
+
 let danger = Danger()
+
+SwiftLint.lint(inline: true)
 
 let editedFiles = danger.git.modifiedFiles + danger.git.createdFiles
 message("These files have changed: \(editedFiles.joined(separator: ", "))")
