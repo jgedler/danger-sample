1173db3188fc79b2ab765fb1084d424149331ed0
Juan Gedler <jgedler@gmail.com>
2025-08-08 13:44:44 -0400

    wip

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -26,4 +26,4 @@ if danger.github != nil {
 //warn(message: "makeMoviesSearchModule() que es esto?", file: "App/Application/DIContainer/AppDIContainer.swift", line: 16)
 
 print("Running Swiftlint on changed files...")
-SwiftLint.lint(inline: true, quiet: false)
+SwiftLint.lint(quiet: false)
