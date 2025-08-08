1fd825f165c267d82f1d080fe985a757eaa24ee3
Juan Gedler <jgedler@gmail.com>
2025-08-08 12:38:55 -0400

    wip

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -24,4 +24,4 @@ if danger.github != nil {
 }
 
 print("Running Swiftlint on changed files...")
-SwiftLint.lint(inline: true, configFile: ".swiftlint.yml")
+SwiftLint.lint(.files(editedFiles), inline: true)
