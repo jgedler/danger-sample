e946798c77fff303fc6d36bd55b1c31d60edf497
Juan Gedler <jgedler@gmail.com>
2025-08-08 14:39:11 -0400

    wip

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -23,6 +23,8 @@ if danger.github != nil {
     }
 }
 
+warn(message: "Estamos seguros que no utilizamos este import?", file: "App/Application/DIContainer/AppDIContainer.swift", line: 9)
+
 warn(message: "Estamos usando esto?. Veo que esta declarado pero no veo que se este utilizando", file: "DevPods/MoviesSearch/MoviesSearch/Module/Presentation/MoviesScene/MoviesList/View/MoviesListViewController.swift", line: 41)
 
 //print("Running Swiftlint on changed files...")
